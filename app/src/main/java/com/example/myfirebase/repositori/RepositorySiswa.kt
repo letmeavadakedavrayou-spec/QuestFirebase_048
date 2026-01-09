ecfae60a8f1aba9a13e9faf840c491d09550c4f2
nachool <nachma.olivia1394@gmail.com>
2026-01-09 18:59:35 +0700

    nnambah repositori siswa buat ngapus, edit, dan detail

--- app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
@@ -7,28 +7,15 @@ import kotlinx.coroutines.tasks.await
 interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
+
+    suspend fun getSatuSiswa(id: Long): Siswa?
+    suspend fun editSatuSiswa(siswa: Siswa)
+    suspend fun hapusSatuSiswa(id: Long)
 }
 
 class FirebaseRepositorySiswa : RepositorySiswa {
     private val db = FirebaseFirestore.getInstance()
     private val collection = db.collection("siswa")
-
-    /*override suspend fun getDataSiswa(): List<Siswa> {
-        return try {
-            collection.get().await().documents.map { doc ->
-                Siswa(
-                    id = doc.getLong("id") ?: 0,
-                    nama = doc.getString("nama") ?: "",
-                    alamat = doc.getString("alamat") ?: "",
-                    telpon = doc.getString("telpon") ?: ""
-                )
-            }
-        } catch (e: Exception) {
-            emptyList()
-        }
-    }*/
-
-
 
     override suspend fun getDataSiswa(): List<Siswa> {
         return try {
@@ -58,4 +45,42 @@ class FirebaseRepositorySiswa : RepositorySiswa {
         )
         docRef.set(data).await()
     }
+
+    override suspend fun getSatuSiswa(id: Long): Siswa? {
+        return try {
+            val query = collection.whereEqualTo("id", id).get().await()
+            query.documents.firstOrNull()?.let { doc ->
+                Siswa(
+                    id = doc.getLong("id") ?: 0L,
+                    nama = doc.getString("nama") ?: "",
+                    alamat = doc.getString("alamat") ?: "",
+                    telpon = doc.getString("telpon") ?: ""
+                )
+            }
+        } catch (e: Exception) {
+            android.util.Log.e("FIREBASE_ERROR", "Gagal baca data siswa: ${e.message}")
+            null
+        }
+    }
+
+    override suspend fun editSatuSiswa(siswa: Siswa) {
+        val docQuery = collection.whereEqualTo("id", siswa.id).get().await()
+        val docId = docQuery.documents.firstOrNull()?.id ?: return
+        collection.document(docId).set(
+            mapOf(
+                "id" to siswa.id,
+                "nama" to siswa.nama,
+                "alamat" to siswa.alamat,
+                "telpon" to siswa.telpon
+            )
+        ).await()
+    }
+
+    override suspend fun hapusSatuSiswa(id: Long) {
+        val docQuery = collection.whereEqualTo("id", id).get().await()
+        val docId = docQuery.documents.firstOrNull()?.id ?: return
+        collection.document(docId).delete().await()
+    }
+
+
 }
