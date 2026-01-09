b31fd43f31c87daf51e31f427e1dd3ec7d252e0d
nachool <nachma.olivia1394@gmail.com>
2026-01-09 19:04:01 +0700

    ngapus komen error build gradle nya

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,4 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
-    // HAPUS BARIS INI: alias(libs.plugins.kotlin.compose) apply false
 }
