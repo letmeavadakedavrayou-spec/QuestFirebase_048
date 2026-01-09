ceee60aec9ca7677076e708efacd779cd2b07d47
nachool <nachma.olivia1394@gmail.com>
2026-01-09 18:51:30 +0700

    nambah view model buat edit

--- app/src/main/java/com/example/myfirebase/view/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/view/viewmodel/EditViewModel.kt
@@ -1,4 +1,60 @@
 package com.example.myfirebase.view.viewmodel
 
-class EditViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.DetailSiswa
+import com.example.myfirebase.modeldata.Siswa
+import com.example.myfirebase.modeldata.UIStateSiswa
+import com.example.myfirebase.modeldata.toDataSiswa
+import com.example.myfirebase.modeldata.toUIStateSiswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import com.example.myfirebase.view.route.DestinasiEdit
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+    private val _uiStateSiswa = MutableStateFlow(UIStateSiswa())
+    val uiStateSiswa: StateFlow<UIStateSiswa> = _uiStateSiswa.asStateFlow()
+
+    private val itemId: String = savedStateHandle.get<String>(DestinasiEdit.itemIdArg)
+        ?: error("ID siswa tidak ditemukan")
+
+    init {
+        loadSiswaData()
+    }
+
+    private fun loadSiswaData() {
+        viewModelScope.launch {
+            val siswa = repositorySiswa.getSatuSiswa(itemId.toLong())
+            siswa?.let {
+                _uiStateSiswa.value = it.toUIStateSiswa(isEntryValid = true)
+            }
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        _uiStateSiswa.value = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    private fun validasiInput(detailSiswa: DetailSiswa): Boolean {
+        return with(detailSiswa) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    suspend fun updateSiswa() {
+        if (validasiInput(uiStateSiswa.value.detailSiswa)) {
+            val siswa = uiStateSiswa.value.detailSiswa.toDataSiswa().copy(id = itemId.toLong())
+            repositorySiswa.editSatuSiswa(siswa)
+        }
+    }
 }
