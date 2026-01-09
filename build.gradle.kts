d8d63a881a9a58bc80f13f44322c7e9aa5c822e6
nachool <nachma.olivia1394@gmail.com>
2026-01-09 18:50:27 +0700

    benerin build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.google.services) apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    // HAPUS BARIS INI: alias(libs.plugins.kotlin.compose) apply false
 }
